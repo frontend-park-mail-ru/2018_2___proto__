d454f4e7c035992663eb583084a59fbc00eaafc6
M. W. Bäcker <nightparis@mail.ru>
2018-09-10 16:38:41 +0300

    Правки стилей, доработка код-стайла

--- server.js
+++ server.js
@@ -1,47 +1,44 @@
 exports.HandleRequest = HandleRequest;
 
-const debug = require("debug");
-const http = require("http");
-const fs = require("fs");
-
+const debug = require('debug');
+const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
-const log = debug("*");
-const root = "./public";
-const index = "/views/index.html";
+const log = debug('*');
+const root = './public';
+const index = '/views/index.html';
 const fileNotFindErrorCode = -2;
 
-
 function HandleRequest(req, res) {
-    log("request: %s", req.url);
+  log('request: %s', req.url);
 
-    const requestedFilePath = (req.url == "/") ? `${root}/${index}` : `${root}${req.url}`;
-    let fileContents;
+  const requestedFilePath = req.url == '/' ? `${root}/${index}` : `${root}${req.url}`;
+  let fileContents;
 
-    try {
-        fileContents = fs.readFileSync(requestedFilePath);
-    } catch (err) {
-        switch (err.errno) {
-            case fileNotFindErrorCode:
-                res.statusCode = 404;
-                break;
+  try {
+    fileContents = fs.readFileSync(requestedFilePath);
+  } catch (err) {
+    switch (err.errno) {
+      case fileNotFindErrorCode:
+        res.statusCode = 404;
+        break;
 
-            default:
-                res.statusCode = 400;
-                break;
-        }
-
-        log("error: %s", err.message)
-        fileContents = err.message;
+      default:
+        res.statusCode = 400;
+        break;
     }
 
-    res.write(fileContents);
-    res.end();
-}
+    log('error: %s', err.message);
+    fileContents = err.message;
+  }
 
+  res.write(fileContents);
+  res.end();
+}
 
 const server = http.createServer(HandleRequest);
 
 server.listen(port, () => {
-    log("server started on port %s", port)
+  log('server started on port %s', port);
 });
