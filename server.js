d131b1116c05c1ec256b0468162103e176d9cc08
M. W. Bäcker <nightparis@mail.ru>
2018-09-14 02:29:19 +0300

    Правки стилей и сервера; подготовка к SPA

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ exports.HandleRequest = HandleRequest;
 
 const debug = require("debug");
 const http = require("http");
+const path = require("path");
 const fs = require("fs");
 
 const port = 3000;
@@ -12,9 +13,14 @@ const fileNotFindErrorCode = -2;
 
 function HandleRequest(req, res) {
 	log("request: %s", req.url);
+	let requestedFilePath;
 
-	const requestedFilePath =
-		req.url == "/" ? `${root}/${index}` : `${root}${req.url}`;
+	if (path.extname(req.url) == ".html") {
+		requestedFilePath = req.url == "/" ? `${root}/${index}` : `${root}/views${req.url}`
+	} else {
+		requestedFilePath = req.url == "/" ? `${root}/${index}` : `${root}${req.url}`
+	}
+
 	let fileContents;
 
 	try {
