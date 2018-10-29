23dd9727eb000772cd639d5d9caf185d917c5f32
M.W. Bäcker <nightparis@mail.ru>
2018-10-29 15:52:19 +0300

    Правка для соответствия валидации на бэкенде

--- app/modules/constants.js
+++ app/modules/constants.js
@@ -1,4 +1,4 @@
 export const regexLogin = /^[a-zA-Z]/;
 export const regexEmail = /\S+@\S+/;
-export const regexPass = /[0-9a-zA-Z]{6,}/;
+export const regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/;
 export const backend = "https://rasseki.pro";
