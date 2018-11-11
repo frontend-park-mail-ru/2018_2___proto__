46c2045721f6fd21259b2bd6d752974ca02572f3
M.W. Bäcker <nightparis@mail.ru>
2018-11-11 20:39:16 +0300

    Ищу ошибки на лидерборде [3]

--- app/components/leaderboard/leaderboard.js
+++ app/components/leaderboard/leaderboard.js
@@ -12,6 +12,9 @@ export default class LeaderboardComponent extends BaseComponent {
 		this.template = template;
 		this._currentPage = "1";
 		this._offset = 0;
+		http.getLeaderboard(this._offset, 10).then((data) => {
+			this.render({ ...{ preloader: false }, data });
+		});
 	}
 
 	render(context) {
