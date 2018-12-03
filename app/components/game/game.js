b384270b892dc9ead682ee16665199021ef82e6b
Artem Andryukhov <mouseartiom@yandex.ru>
2018-12-03 15:18:40 +0300

    first game attempts

--- app/components/game/game.js
+++ app/components/game/game.js
@@ -2,19 +2,22 @@ import "./game.scss";
 import template from "./game.hbs";
 import BaseComponent from "../baseComponent";
 import ButtonComponent from "../button/button";
-import GameScene from "../../game/engine/core/gameScene.ts";
-import GameObject from "../../game/engine/core/gameObject.ts";
-import HeroBehaviour from "../../game/gameplay/heroBehaviour.ts";
-import LocalServer from "../../game/serverSide/localGameServer.ts";
+import Canvas from "../../game/animation/render";
+// import GameScene from "../../game/engine/core/gameScene.ts";
+// import GameObject from "../../game/engine/core/gameObject.ts";
+// import HeroBehaviour from "../../game/gameplay/heroBehaviour.ts";
+// import LocalServer from "../../game/serverSide/localGameServer.ts";
 
 /**
  * Компонент игры
  */
 
 export default class GameComponent extends BaseComponent {
-	constructor() {
+	constructor(width, height) {
 		super();
 		this.template = template;
+		this.canvas_width = width;
+		this.canvas_height = height;
 	}
 
 	render(context) {
@@ -25,14 +28,56 @@ export default class GameComponent extends BaseComponent {
 		});
 	}
 
+	_renderCanvas() {
+		const [backgroundCanvas] = document.getElementsByClassName("game-screen__background");
+		this.bg = new Canvas(backgroundCanvas, 1000, 600);
+
+		const [heroCanvas] = document.getElementsByClassName("game-screen__heroes");
+		this.heroes = new Canvas(heroCanvas, 1000, 600);
+
+		const background = {
+			image_name: "full-bg",
+			pos_x: 0, // coords of the sprite"s top left point
+			pos_y: 0,
+			width: 1920, // size of the image
+			height: 1080,
+		};
+		this.bg.draw(background);
+
+		const heroLeft = {
+			image_name: "wiz_idle_left",
+			pos_x: 0,
+			pos_y: 100,
+			width: 2248,
+			height: 368,
+			numberOfFrames: 10,
+			ticksPerFrame: 2,
+		};
+
+		// this.heroes.animate(heroLeft);
+
+		const heroRight = {
+			image_name: "knight_idle_right",
+			pos_x: 500, // coords of the sprite"s top left point
+			pos_y: 100,
+			width: 4406, // size of the spritestrip
+			height: 322,
+			numberOfFrames: 12, // amount of frames in spritestrip
+			ticksPerFrame: 2, // speed of animation, more is slower
+		};
+		// this.heroes.animate(heroRight);
+	}
+
+
 	prepareGame() {
-		this._scene = new GameScene();
-		this._heroObject = new GameObject();
-		this._heroObject.AddBehaviour(new HeroBehaviour("test", this._scene));
-		this._heroObject.Enable();
-		this._scene.Render();
-		this._localServer = new LocalServer();
 		this.render({ isReady: true });
-		this._localServer.StartGame();
+		this._renderCanvas();
+		// this._scene = new GameScene();
+		// this._heroObject = new GameObject();
+		// this._heroObject.AddBehaviour(new HeroBehaviour("test", this._scene));
+		// this._heroObject.Enable();
+		// this._scene.Render();
+		// this._localServer = new LocalServer();
+		// this._localServer.StartGame();
 	}
 }
---
+++ app/game/animation/render.js
@@ -0,0 +1,116 @@
+
+export default class CanvasWrapper {
+	constructor(canvas, width, height) {
+		this.canvas = canvas;
+		this.canvas_width = width;
+		this.canvas_height = height;
+		this.canvas.width = width;
+		this.canvas.height = height;
+	}
+
+	animate(options) {
+		let animationSprite;
+
+		function gameLoop() {
+			const id = window.requestAnimationFrame(gameLoop);
+			animationSprite.update();
+			animationSprite.render();
+			return id;
+		}
+
+		// Create sprite sheet
+		const spriteImage = new Image(options.width, options.height);
+		// Load sprite sheet
+		spriteImage.addEventListener("load", gameLoop);
+		spriteImage.src = `/public/sprites/${options.image_name}.png`;
+		console.log(spriteImage.width.valueOf());
+		console.log(spriteImage.height.valueOf());
+
+		// Create sprite
+		animationSprite = this._sprite({
+			context: this.canvas.getContext("2d", { aplha: true }),
+			pos_x: options.pos_x,
+			pos_y: options.pos_y,
+			width: spriteImage.width.valueOf(),
+			height: spriteImage.height.valueOf(),
+			image: spriteImage,
+			numberOfFrames: options.numberOfFrames,
+			ticksPerFrame: options.ticksPerFrame,
+		});
+	}
+
+	draw(options) {
+		/**
+		 * image_name
+		 * pos_x
+		 * pos_y
+		 * width
+		 * height
+		 *
+		 */
+		const that = { ...options };
+		that.context = this.canvas.getContext("2d", { aplha: true });
+		const spriteImage = new Image();
+		spriteImage.src = `/public/sprites/${options.image_name}.png`;
+		spriteImage.onload = () => {
+			that.context.drawImage(
+				spriteImage,
+				0,
+				0,
+				that.width,
+				that.height,
+				that.pos_x,
+				that.pos_y,
+				that.width,
+				that.height,
+			);
+		};
+	}
+
+	_sprite(options) {
+		let frameIndex = 0;
+		let tickCount = 0;
+		const ticksPerFrame = options.ticksPerFrame || 0;
+		const numberOfFrames = options.numberOfFrames || 1;
+
+		const that = { ...options };
+		that.update = () => {
+			tickCount += 1;
+			if (tickCount > ticksPerFrame) {
+				tickCount = 0;
+				// If the current frame index is in range
+				if (frameIndex < numberOfFrames - 1) {
+					// Go to the next frame
+					frameIndex += 1;
+				} else {
+					frameIndex = 0;
+				}
+			}
+		};
+
+		that.render = () => {
+			// Clear the canvas
+			that.context.clearRect(
+				that.pos_x,
+				that.pos_y,
+				that.pos_x + that.width / numberOfFrames,
+				that.pos_y + that.height,
+			);
+
+			// Draw the animation
+			that.context.drawImage(
+				that.image,
+				frameIndex * that.width / numberOfFrames,
+				0,
+				that.width / numberOfFrames,
+				that.height,
+				that.pos_x,
+				that.pos_y,
+				that.width / numberOfFrames,
+				that.height,
+			);
+		};
+
+		return that;
+	}
+}
