5d37ccdd688c3371290991188e499fab46a3162b
Artem Andryukhov <mouseartiom@yandex.ru>
2018-12-18 20:55:42 +0300

    answerButtonComponent added

---
+++ app/components/question/answer_button/button.js
@@ -0,0 +1,36 @@
+import "./button.scss";
+import template from "./button.hbs";
+import BaseComponent from "../../baseComponent";
+
+/**
+ * Компонент game кнопки
+ */
+export default class AnswerButtonComponent extends BaseComponent {
+	constructor() {
+		super();
+		this.template = template;
+		this._context = {
+			text: "",
+			onClick: null,
+			href: null,
+		};
+
+		this.events = {
+			click: this._onClick.bind(this),
+		};
+	}
+
+	/**
+	 * Нажатие на кнопку
+	 * @param {event} event - sender
+	 */
+	_onClick(event) {
+		if (!this._context.href) {
+			event.preventDefault();
+		}
+
+		if (this._context.onClick) {
+			this._context.onClick();
+		}
+	}
+}
--- app/components/question/question.js
+++ app/components/question/question.js
@@ -1,6 +1,7 @@
 import "./question.scss";
 import template from "./question.hbs";
 import BaseComponent from "../baseComponent";
+import AnswerButtonComponent from "./answer_button/button";
 
 export default class QuestionComponent extends BaseComponent {
 	constructor() {
@@ -14,5 +15,33 @@ export default class QuestionComponent extends BaseComponent {
 			answer3: "",
 			answer4: "",
 		};
+	}
+
+	render(context) {
+		super.render(context);
+		this._renderChildren();
+	}
+
+	_renderChildren() {
+		this.renderChild("answer1", AnswerButtonComponent, {
+			text: this._context.answer1,
+			onClick: this._answerEvent(1),
+		});
+		this.renderChild("answer2", AnswerButtonComponent, {
+			text: this._context.answer1,
+			onClick: this._answerEvent(2),
+		});
+		this.renderChild("answer3", AnswerButtonComponent, {
+			text: this._context.answer1,
+			onClick: this._answerEvent(3),
+		});
+		this.renderChild("answer4", AnswerButtonComponent, {
+			text: this._context.answer1,
+			onClick: this._answerEvent(4),
+		});
+	}
+
+	_answerEvent(answerID) {
+		alert(answerID);
 	}
 }
